ae90eb22a7f4eacf3af39580f51d9d971ffa19b3
Jan Horak <janny_b_goode@hotmail.com>
2022-09-15 16:03:05 -0400

    add FeedBackItem component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 import PropTypes from "prop-types";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <h1>my app</h1>
+        <FeedbackItem />
       </div>
     </>
   );
