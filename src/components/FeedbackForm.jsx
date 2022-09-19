611cef1cacb35b99c9c391b2c47ff2cf50cd62b2
Jan Horak <janny_b_goode@hotmail.com>
2022-09-18 22:18:01 -0400

    add input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,24 +3,44 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 
 function FeedbackForm() {
-const [text, setText] = useState('')
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
-const handleTextChange = (e) => {
-  setText(e.target.value)
-}
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if( text !== '' && text.trim().length <= 10){
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null)
+      setBtnDisabled(true);
+    }
+    setText(e.target.value);
+  };
 
   return (
     <Card>
-    <form>
-      <h2>How would you rate your service with us?</h2>
-      {/* @ todo - rating select component */}
-      <div className="input-group">
-        <input onChange={handleTextChange} type='text' placeholder="write a review" value={text} />
-        <Button type="submit">Send</Button>
-      </div>
-    </form>
-  </Card>
-  )
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @ todo - rating select component */}
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
