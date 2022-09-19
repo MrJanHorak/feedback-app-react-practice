a60a1b6aa0bcdce4b86eeac7972ed36a7d941232
Jan Horak <janny_b_goode@hotmail.com>
2022-09-18 23:08:34 -0400

    add className to delete button

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -7,7 +7,7 @@ function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)}>
+      <button className="close" onClick={() => handleDelete(item.id)}>
         <FaTimes color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
