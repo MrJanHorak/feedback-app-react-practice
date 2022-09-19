a20b5235c7ecef469f286346856289891d06db3e
Jan Horak <janny_b_goode@hotmail.com>
2022-09-18 23:04:33 -0400

    add framer-motion animatepresence to comments

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
 
@@ -7,17 +7,39 @@ function FeedbackList({ feedback, handleDelete }) {
     return <p>No Feedback Yet</p>;
   }
   console.log(feedback);
+
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
