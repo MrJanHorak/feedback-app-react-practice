ac8e526efdaa9a423e997f155439c8a29172e68e
Jan Horak <janny_b_goode@hotmail.com>
2022-09-20 14:01:57 -0400

    move handleDelete and addFeedback into context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Header from "./components/Header";
 import FeedBackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import FeedBackData from "./data/FeedBackData";
 import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
 import { FeedbackProvider } from "./context/FeedbackContext";
@@ -13,18 +10,6 @@ import { FeedbackProvider } from "./context/FeedbackContext";
 import PropTypes from "prop-types";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedBackData);
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
   return (
     <FeedbackProvider>
       <Router>
@@ -36,9 +21,9 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackForm />
                   <FeedbackStats />
-                  <FeedBackList handleDelete={deleteFeedback} />
+                  <FeedBackList />
                 </>
               }
             ></Route>
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,19 +1,39 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback ] = useState([
+  const [feedback, setFeedback] = useState([
     {
       id: 1,
       text: "This item is from Context",
-      rating: 10
+      rating: 10,
+    },
+  ]);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  ])
-  return <FeedbackContext.Provider value={{
-    feedback
-  }}>{children}</FeedbackContext.Provider>;
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
 };
 
-
-export default FeedbackContext
+export default FeedbackContext;
